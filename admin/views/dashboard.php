6d7a4b5e7ba5ae19c28db2b0809c2e3f00a2e3f1
wkkkuu <hinm.aaaa@gmail.com>
2024-11-14 00:57:30 +0700

    create folder client & update views of admin

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -7,6 +7,10 @@
 </head>
 <body>
 <h1>TRANG QUẢN TRỊ WEBSITE</h1>
-<a href=""><Button>NÚT QUẢN TRỊ</Button></a>
+<a href=""><Button>QUẢN LÝ DANH MỤC</Button></a>
+<a href=""><Button>QUẢN LÝ SẢN PHẨM</Button></a>
+<a href=""><Button>QUẢN LÝ ĐƠN HÀNG</Button></a>
+<a href=""><Button>QUẢN LÝ BÌNH LUẬN</Button></a>
+<a href=""><Button>QUẢN LÝ NGƯỜI DÙNG</Button></a>
 </body>
 </html>
