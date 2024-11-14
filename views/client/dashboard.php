2c7a459f4089aa3dd2588e8a9dcb034041aedbeb
wkkkuu <hinm.aaaa@gmail.com>
2024-11-14 13:15:21 +0700

    test

--- views/client/dashboard.php
+++ views/client/dashboard.php
@@ -46,6 +46,29 @@
                 BANNER
             </div>
         </header>
+
+        <div id="content">
+            <h1>MỚI & NỔI BẬT</h1>
+            <h1>NAM</h1>
+            <h1>NỮ</h1>
+            <h1>TRẺ EM</h1>
+        </div>
+
+        <footer id="footer">
+            <div class="left-section">
+                <div class="contact-info">
+                    <p><b>Địa chỉ: </b>FPT Polytechnic Hà Nội</p>
+                    <p><b>Liên hệ: </b>+977907877</p>
+                    <p><b>Email: </b>contact@nhom4.com</p>
+                </div>
+            </div>
+
+            <div class="right-section">
+                <p>Copyright <b>&copy;</b> DA1 by Nhóm 4</p>
+                <br>
+                <p><b>Chính sách bảo mật | Điều khoản sử dụng</b></p>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
