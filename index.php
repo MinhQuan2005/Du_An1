38992c897543ca5c2a2713339f6503145f083a54
wkkkuu <hinm.aaaa@gmail.com>
2024-11-16 11:45:23 +0700

    update admin

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
 // Require toàn bộ file Commons
-require_once './commons/env.php';
-require_once './commons/function.php';
+require_once './commons/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
+
 
  // Require toàn bộ file Controllers
 require_once './controllers/client/dashboardController.php';
+require_once './controllers/admin/adminDashboardController.php';
+
 
  // Require toàn bộ file Models
  
 
+
  // Route
 $act = $_GET['act'] ?? '/';
 match ($act) {
-    // Trang chủ quản trị
+    // Trang chủ
     '/' => (new dashboardController)->dashboard(),
+
+    // Trang quản trị
+    '/admin' => (new adminDashboardController)->adminDashboard(),
     
 }
 ?>
---
+++ views/admin/adminDashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRANG QUẢN TRỊ</title>
+    <link rel="stylesheet" href="css/admin/adminDashboard.css">
+</head>
+<body>
+    <div class="dashboard-container">
+        <h2>BẢNG ĐIỀU KHIỂN</h2>
+        <div class="menu">
+            <ul>
+                <li><a href="#">QUẢN LÝ DANH MỤC</a></li>
+                <li><a href="#">QUẢN LÝ SẢN PHẨM</a></li>
+                <li><a href="#">QUẢN LÝ NGƯỜI DÙNG</a></li>
+                <li><a href="#">QUẢN LÝ BÌNH LUẬN</a></li>
+                <li><a href="#">QUẢN LÝ ĐƠN HÀNG</a></li>
+                <li><a href="#">THỐNG KÊ VÀ BÁO CÁO</a></li>
+                <li><a href="account/logout.php">ĐĂNG XUẤT</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <p>Tại đây, bạn có thể quản lý các danh mục, sản phẩm, người dùng, bình luận, đơn hàng và xem thống kê, báo cáo.</p>
+        </div>
+    </div>
+</body>
+</html>
