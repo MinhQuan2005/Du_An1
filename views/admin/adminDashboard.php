fb98040bc5f164204ed6e13256fe374589c28693
wkkkuu <hinm.aaaa@gmail.com>
2024-11-16 16:26:04 +0700

    update quản lý danh mục

--- views/admin/adminDashboard.php
+++ views/admin/adminDashboard.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TRANG QUẢN TRỊ</title>
-    <link rel="stylesheet" href="css/admin/adminDashboard.css">
+    <link rel="stylesheet" href="../../css/admin/adminDashboard.css">
 </head>
 <body>
     <div class="dashboard-container">
         <h2>BẢNG ĐIỀU KHIỂN</h2>
         <div class="menu">
             <ul>
-                <li><a href="#">QUẢN LÝ DANH MỤC</a></li>
+                <li><a href="../admin/manage_categories.php">QUẢN LÝ DANH MỤC</a></li>
                 <li><a href="#">QUẢN LÝ SẢN PHẨM</a></li>
                 <li><a href="#">QUẢN LÝ NGƯỜI DÙNG</a></li>
                 <li><a href="#">QUẢN LÝ BÌNH LUẬN</a></li>
                 <li><a href="#">QUẢN LÝ ĐƠN HÀNG</a></li>
                 <li><a href="#">THỐNG KÊ VÀ BÁO CÁO</a></li>
-                <li><a href="account/logout.php">ĐĂNG XUẤT</a></li>
+                <li><a href="../../account/logout.php">ĐĂNG XUẤT</a></li>
             </ul>
         </div>
         <div class="content">
