57300edf3fa007068c868142cdf110f292c3f95e
MinhQuan2005 <quandmph50159@gmail.com>
2024-11-16 21:16:52 +0700

    update file index

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
-require_once './commons/function.php';
+// Require toàn bộ file commons
+require_once './commons/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
+
+
+// Require toàn bộ file controllers
+require_once './controllers/client/dashboardController.php';
+require_once './controllers/admin/adminDashboardController.php';
 require_once './controllers/client/userController.php';
-require_once './controllers/admin/dashboardController.php';
 require_once './controllers/client/dashboardController.php';
 
+
+// Require toàn bộ file models
+
+
+// Route
 $act = $_GET['act'] ?? '/';
 
 session_start();
 $is_admin = $_SESSION['user']['is_admin'] ?? 0;
 
 match ($act) {
-    '/' => $is_admin ? :
+    '/' => $is_admin ? 
+        (new adminDashboardController): 
         (new dashboardController())->dashboard(),
     'register' => (new UserController())->register(),
     'login' => (new UserController())->login(),
