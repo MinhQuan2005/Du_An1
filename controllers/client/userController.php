85ff785edbc96fcd52a59068627921ea5019c4a0
MinhQuan2005 <quandmph50159@gmail.com>
2024-11-16 20:38:40 +0700

    update all folder account

---
+++ controllers/client/userController.php
@@ -0,0 +1,87 @@
+<?php
+
+require_once './models/client/user.php';
+require_once './commons/function.php';
+
+class UserController {
+    private $userModel;
+
+    private $pdo;
+
+
+    public function __construct() {
+        
+        $host = 'localhost';
+        $user="root";
+        $db = 'x_shop';   
+        $pass = '123123';   
+        $charset = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $e) {
+            die("Lỗi kết nối: " . $e->getMessage());
+        }
+
+       
+        $this->userModel = new User($this->pdo);
+    }
+    
+
+    public function register() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            if ($this->userModel->register($username, $email, $password)) {
+                header("Location: account/login.php");
+                exit();
+            } else {
+                echo "Error in registration!";
+            }
+        }
+    }
+
+    public function login() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+    
+            $user = $this->userModel->login($username, $password);
+    
+            if ($user) {
+                session_start();
+                $_SESSION['user'] = $user;
+                $_SESSION['login_message'] = "Đăng nhập thành công!";
+    
+                if ($user['is_admin']) {
+                    header("Location: index.php?act=admin");
+                } else {
+                    header("Location: index.php?act=home");
+                }
+                exit();
+            } else {
+                session_start();
+                $_SESSION['login_message'] = "Sai thông tin đăng nhập hoặc tài khoản không tồn tại";
+                header("Location: account/login.php");
+                exit();
+            }
+        }
+    }
+    
+
+    public function logout() {
+        session_start();
+        session_destroy();
+        header("Location: account/login.php");
+    }
+}
+?>
